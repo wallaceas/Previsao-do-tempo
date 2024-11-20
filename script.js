e6e53169e45b19316527e5889eb208d8b3aa79d5
wallaceas <133448665+wallaceas@users.noreply.github.com>
2024-11-20 19:26:21 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// VARIÁVEIS => Um espaço da memória do computador que guardamos algo (um numero, uma letra, um texto, uma imagem)
+// FUNÇÃO => Um trecho de código que só é executado quando é chamado
+
+
+const key = "323889e7e1b0fd49b969253ad4f532e4"
+
+function colocarDadosNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "ºC"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+
+async function buscarCidade(cidade){
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`)
+    .then(resposta => resposta.json())
+    colocarDadosNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+    buscarCidade(cidade)
+}
